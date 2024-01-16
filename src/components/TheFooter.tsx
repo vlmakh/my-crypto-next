4a0542e2b889e7f6ab0644843f4e19ac31f3f174
Vlad Makhnenko <vlmakh@gmail.com>
2024-01-16 08:05:03 +0200

    Update TheFooter.tsx

--- src/components/TheFooter.tsx
+++ src/components/TheFooter.tsx
@@ -3,24 +3,16 @@ import Image from "next/image";
 export const TheFooter = () => {
   return (
     <footer className="flex gap-4 justify-center items-center">
-      <p> VM</p>
+      <p>2024</p>
 
       <div className="flex justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
         <a
           className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://vlad-makhnenko.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          By{" "}
-          <Image
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            className="dark:invert"
-            width={100}
-            height={24}
-            priority
-          />
+          By VM
         </a>
       </div>
     </footer>
