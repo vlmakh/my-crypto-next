7a3165132ef2fc710c0a8d633d500402efc4932c
Vlad Makhnenko <vlmakh@gmail.com>
2024-01-17 21:54:20 +0200

    Update CoinChart.tsx

--- src/components/CoinChart.tsx
+++ src/components/CoinChart.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react';
-import { historicalChart } from '@/utils/fetchCoinList';
+import { useState, useEffect } from "react";
+// import { historicalChart } from '@/utils/fetchCoinList';
 import {
   LineChart,
   Line,
@@ -9,8 +9,8 @@ import {
   XAxis,
   YAxis,
   Tooltip,
-} from 'recharts';
-import { ICoin } from '@/types';
+} from "recharts";
+// import { ICoin } from '@/types';
 
 // const TooltipContent = (props: { active: any; payload: { payload: any; }[]; }) => {
 //   if (!props.active || !props.payload || !props.payload.payload) {
@@ -28,52 +28,71 @@ import { ICoin } from '@/types';
 // };
 
 type Props = {
-    id: string
+  id: string;
+};
+
+type CoinData = any[];
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    "X-API-KEY": "r9FNSR3h4KySY93Gz9AJaYZMII+7fsxA7b1mvlPVQhY=",
+  },
+};
+
+async function historicalChart(id: string, period = "1y") {
+  const response = await fetch(
+    `https://openapiv1.coinstats.app/coins/${id}/charts?period=${period}`,
+    options
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return response.json();
 }
 
-type CoinData = any[]
+export const CoinChart = ({ id }: Props) => {
+  const [coinData, setCoinData] = useState<CoinData>([]);
+  const [period, setPeriod] = useState("1y");
 
-
-export const CoinChart = ({id}: Props) => {
-    const [coinData, setCoinData] = useState<CoinData>([]);
-    const [period, setPeriod] = useState('24h');
-
-    useEffect(() => {
+  useEffect(() => {
     const newCoinData: CoinData = [];
 
     historicalChart(id, period)
-      .then(coinPrices => {
+      .then((coinPrices) => {
         for (let i = 0; i < coinPrices.length; i++) {
           newCoinData.push({
-            time: (coinPrices[i][0]),
+            time: coinPrices[i][0],
             price: coinPrices[i][1],
           });
-          }
-          
-          console.log(newCoinData);
+        }
+
+        // console.log(newCoinData);
 
         setCoinData(newCoinData);
       })
-      .catch(error => console.log(error.message));
+      .catch((error) => console.log(error.message));
   }, [id, period]);
 
   return (
-      <div>
-          <LineChart data={coinData} width={768} height={400}>
-          <Line
-            dot={false}
-            stroke="orange"
-            strokeWidth={2}
-            dataKey={'price'}
-            isAnimationActive={false}
-          />
-          <CartesianGrid stroke="grey" strokeDasharray="5 5" />
-          <XAxis dataKey={'time'} />
-          <YAxis unit="$" />
+    <div className="">
+      <LineChart data={coinData} width={768} height={400}>
+        <Line
+          dot={false}
+          stroke="orange"
+          strokeWidth={2}
+          dataKey={"price"}
+          isAnimationActive={false}
+        />
+        <CartesianGrid stroke="grey" strokeDasharray="5 5" />
+        <XAxis dataKey={"time"} />
+        <YAxis unit="$" />
 
-          {/* <Tooltip isAnimationActive={false} content={<TooltipContent />} /> */}
-        </LineChart>
-
+        {/* <Tooltip isAnimationActive={false} content={<TooltipContent />} /> */}
+      </LineChart>
     </div>
-  )
-}
+  );
+};
