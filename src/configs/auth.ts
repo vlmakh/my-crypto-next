8f931331a342ac7c312dae812ac161cdf047a86b
Vlad Makhnenko <vlmakh@gmail.com>
2024-01-20 17:48:56 +0200

    fix middlware

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,5 +1,8 @@
 import type { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
+import { Adapter } from "next-auth/adapters";
 
 export const authConfig: AuthOptions = {
   providers: [
@@ -8,6 +11,13 @@ export const authConfig: AuthOptions = {
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
   ],
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n') ?? undefined,
+    }),
+  }) as Adapter,
   pages: {
     signIn: "/signin",
   },
