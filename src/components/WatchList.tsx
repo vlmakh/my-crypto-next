f2fa591f4cf7db229f243c23551eb51c900e8a35
Vlad Makhnenko <vlmakh@gmail.com>
2024-01-20 13:02:25 +0200

    Update WatchList.tsx

--- src/components/WatchList.tsx
+++ src/components/WatchList.tsx
@@ -5,7 +5,7 @@ import { CoinListItem } from "./CoinListItem";
 import { ICoin } from "@/types";
 import { userWatchList } from "@/utils/fetchCoinList";
 
-const query: string[] = ['bitcoin', 'karlsen', 'huobi-token'];
+const query: string[] = ["bitcoin", "karlsen", "huobi-token"];
 
 export const WatchList = () => {
   const [userList, setUserList] = useState<ICoin[] | never[]>([]);
@@ -18,20 +18,26 @@ export const WatchList = () => {
         setUserList(data);
       })
       .catch((error) => console.log("Network error"));
-    
+
     return () => {
       controller.abort();
     };
   }, []);
 
   return (
-    <ul>
-      {userList.length > 0 &&
-        userList
-          .sort((a: { rank: number }, b: { rank: number }) => a.rank - b.rank)
-          .map(
-            (coin: ICoin) => coin && <CoinListItem key={coin.id} coin={coin} />
-          )}
-    </ul>
+    <>
+      {userList.length > 0 ? (
+        <ul>
+          {userList
+            .sort((a: { rank: number }, b: { rank: number }) => a.rank - b.rank)
+            .map(
+              (coin: ICoin) =>
+                coin && <CoinListItem key={coin.id} coin={coin} />
+            )}
+        </ul>
+      ) : (
+        <p className="text-center">Watchlist is empty</p>
+      )}
+    </>
   );
 };
