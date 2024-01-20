85d79bf622c932f2c0ddd226f9739c8aa6cc2bee
Vlad Makhnenko <vlmakh@gmail.com>
2024-01-20 18:17:34 +0200

    fix firebase

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -15,7 +15,7 @@ export const authConfig: AuthOptions = {
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n') ?? undefined,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
     }),
   }) as Adapter,
   pages: {
--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getApp, getApps, initializeApp } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -11,8 +11,8 @@ const firebaseConfig = {
   appId: process.env.FB_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+// const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db };
+export { app, db };
