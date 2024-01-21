701d9477121f4ff5940d1d01639c33707150e7ee
Vlad Makhnenko <vlmakh@gmail.com>
2024-01-21 20:25:51 +0200

    Update page.tsx

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,12 +1,15 @@
 import { SigninGoogleBtn } from "@/components/ui/SigninGoogleBtn";
 import { SigninEmailForm } from "@/components/ui/SigninEmailForm";
-// import { getServerSession } from "next-auth";
-// import { authConfig } from "@/configs/auth";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authConfig } from "@/configs/auth";
 
 export default async function SigninPage() {
-  // const session = await getServerSession(authConfig);
+  const session = await getServerSession(authConfig);
 
-  // console.log(session)
+  {
+    session?.user && redirect(`/watchlist`);
+  }
 
   return (
     <div className="text-center">
