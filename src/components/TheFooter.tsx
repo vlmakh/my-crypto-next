aec66c23649ddec1cf49cf2b6bfcdb8e90ae4c7a
Vlad Makhnenko <vlmakh@gmail.com>
2024-01-27 20:18:57 +0200

    update Footer

--- src/components/TheFooter.tsx
+++ src/components/TheFooter.tsx
@@ -1,21 +1,18 @@
-import Image from "next/image";
+import { LogoVM } from "./ui/LogoVM";
 
 export const TheFooter = () => {
   return (
     <footer className="flex gap-4 justify-center items-center py-2">
       <p>2024</p>
-
-      <div className="flex justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-        <a
-          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-          href="https://vlad-makhnenko.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          By
-          <Image src="/vm_logo.svg" alt="VM" width={40} height={21} />
-        </a>
-      </div>
+      <span>By</span>
+      <a
+        className="pointer-events-none lg:pointer-events-auto lg:p-0 transition-colors hover:text-yellow-500"
+        href="https://vlad-makhnenko.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >        
+        <LogoVM />
+      </a>
     </footer>
   );
 };
