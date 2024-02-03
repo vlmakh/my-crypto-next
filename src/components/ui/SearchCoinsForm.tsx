e3c61a8e6eb14f6ae623cd7ea58e884b67789114
Vlad Makhnenko <vlmakh@gmail.com>
2024-02-03 21:34:15 +0200

    add debounce

--- src/components/ui/SearchCoinsForm.tsx
+++ src/components/ui/SearchCoinsForm.tsx
@@ -1,32 +1,32 @@
 'use client';
 
-import { useState } from 'react';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export const SearchCoinsForm = () => {
   const searchParams = useSearchParams();
-  const [valueQuery, setValueQuery] = useState(searchParams.get('query')?.toString() ?? '');
   const pathname = usePathname();
   const { replace } = useRouter();
 
   const params = new URLSearchParams(searchParams);
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const query = e.target.value;
-    setValueQuery(query);
+  const handleInput = useDebouncedCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const query = e.target.value;
 
-    if (query) {
-      params.set('query', query);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
-  };
+      if (query) {
+        params.set('query', query);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString()}`);
+    },
+    400
+  );
 
   const handleDelete = () => {
     params.delete('query');
     replace(`${pathname}?${params.toString()}`);
-    setValueQuery('');
   };
 
   return (
@@ -35,13 +35,13 @@ export const SearchCoinsForm = () => {
         <input
           onChange={handleInput}
           placeholder="by coin name or symbol"
-          value={valueQuery}
+          defaultValue={searchParams.get('query')?.toString()}
           autoComplete="off"
           className="w-full rounded-md border-0 bg-white/5 px-2 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-yellow-500 sm:text-sm sm:leading-6"
         />
 
         <button
-          type="button"
+          type="reset"
           className="absolute right-2 px-2 py-1 text-transparent transition-colors group-hover:text-gray-500"
           onClick={handleDelete}
         >
