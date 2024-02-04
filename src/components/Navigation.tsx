bf9a0966ddcceb75d5fe987718deaed2691064ed
Vlad Makhnenko <vlmakh@gmail.com>
2024-02-04 09:29:50 +0200

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,34 +1,33 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { navlinks } from "@/data/navlinks";
-import { usePathname } from "next/navigation";
-import { useUserStore } from "@/configs/store";
+import Link from 'next/link';
+import { navlinks } from '@/data/navlinks';
+import { usePathname } from 'next/navigation';
+import { useUserStore } from '@/configs/store';
 
 export const Navigation = () => {
   const pathname = usePathname();
-  const uid = useUserStore((state) => state.uid);
-  const signout = useUserStore((state) => state.signout);
+  const uid = useUserStore(state => state.uid);
+  const signout = useUserStore(state => state.signout);
   const linkClass =
-    "group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30";
+    'group text-lg font-semibold rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30';
   const spanClass =
-    "inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none group-hover:text-yellow-500";
-
+    'inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none group-hover:text-yellow-500';
 
   return (
-    <div className="flex gap-2 justify-center">
-      {navlinks.map((link) => {
+    <div className="flex justify-center gap-2">
+      {navlinks.map(link => {
         const isActive = pathname === link.href;
 
         return (
           <Link
             key={link.id}
             href={link.href}
-            className={`${linkClass} ${isActive ? "text-yellow-500" : ""}`}
+            className={`${linkClass} ${isActive ? 'text-yellow-500' : ''}`}
           >
-            <p className="text-xl font-semibold">
+            <p>
               {link.label}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none group-hover:text-yellow-500">
+              <span className="inline-block transition-transform group-hover:translate-x-1 group-hover:text-yellow-500 motion-reduce:transform-none">
                 -&gt;
               </span>
             </p>
@@ -40,10 +39,10 @@ export const Navigation = () => {
         <Link
           href="/watchlist"
           className={`${linkClass} ${
-            pathname === "/watchlist" ? "text-yellow-500" : ""
+            pathname === '/watchlist' ? 'text-yellow-500' : ''
           }`}
         >
-          <p className="text-xl font-semibold">
+          <p>
             Watchlist
             <span className={spanClass}>-&gt;</span>
           </p>
@@ -51,12 +50,8 @@ export const Navigation = () => {
       )}
 
       {uid ? (
-        <Link
-          href="#"
-          className={linkClass}
-          onClick={() => signout()}
-        >
-          <p className="text-xl font-semibold">
+        <Link href="#" className={linkClass} onClick={() => signout()}>
+          <p>
             Sign Out
             <span className={spanClass}>-&gt;</span>
           </p>
@@ -65,10 +60,10 @@ export const Navigation = () => {
         <Link
           href="/signin"
           className={`${linkClass} ${
-            pathname === "/signin" ? "text-yellow-500" : ""
+            pathname === '/signin' ? 'text-yellow-500' : ''
           }`}
         >
-          <p className="text-xl font-semibold">
+          <p>
             SignIn
             <span className={spanClass}>-&gt;</span>
           </p>
