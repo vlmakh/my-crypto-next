5214a65ebe19a1feef8d81fac234f7c9494f6da1
Vlad Makhnenko <vlmakh@gmail.com>
2024-02-14 21:52:53 +0200

    Update TheHeader.tsx

--- src/components/TheHeader.tsx
+++ src/components/TheHeader.tsx
@@ -1,19 +1,20 @@
-import Link from "next/link";
+import Link from 'next/link';
 import Image from 'next/image';
-import { Navigation } from "./Navigation";
+import { Navigation } from './Navigation';
 
 export const TheHeader = () => {
   return (
-    <header className="border-b-2 flex items-center px-5">
-
-      <Link href="/"><Image
-        src="/logo512.webp"
-        alt=""
-        width={40}
-        height={40}
-        className="mr-5"
-        priority
-      /></Link>
+    <header className="flex items-center border-b-2 px-5">
+      <Link href="/" className="group py-2">
+        <Image
+          src="/logo512.webp"
+          alt="Logo"
+          width={40}
+          height={40}
+          className="mr-5 transition-transform group-hover:scale-110"
+          priority
+        />
+      </Link>
 
       <Navigation />
     </header>
