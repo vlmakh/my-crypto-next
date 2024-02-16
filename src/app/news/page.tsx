1c3ede0a4f72724dabc5bfceb0eff6865f79ff52
Vlad Makhnenko <vlmakh@gmail.com>
2024-02-16 06:13:03 +0200

    upd News

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,17 +1,16 @@
 import { fetchNewsList } from '@/utils/fetchCoinList';
 import type { INewsItem } from '@/types';
 import { Container } from '@/components/Container';
+import { formatMStoDate } from '@/utils/formatDate';
 
 export default async function NewsPage() {
   const totalNewsList = await fetchNewsList(1);
 
   return (
     <Container>
-      <h1 className="text-center">News</h1>
-
       <ul>
         {totalNewsList.map((item: INewsItem) => (
-          <li key={item.id} className="group border-b-2">
+          <li key={item.id} className="group border-b-2 py-2">
             <a
               href={item.link}
               target="_blank"
@@ -20,7 +19,12 @@ export default async function NewsPage() {
               {item.title}
             </a>
 
-            <p>{item.description}</p>
+            <p className="text-justify text-sm">{item.description}</p>
+
+            <div className="mt-2 flex justify-between text-sm">
+              <p>{item.source}</p>
+              <p>{formatMStoDate(item.feedDate)}</p>
+            </div>
           </li>
         ))}
       </ul>
