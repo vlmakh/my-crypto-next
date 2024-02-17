eef73c362f4a450b4eceaab9ee30fb3074c865af
Vlad Makhnenko <vlmakh@gmail.com>
2024-02-17 08:32:48 +0200

    update BurgerMenu

--- src/components/ui/BurgerMenu.tsx
+++ src/components/ui/BurgerMenu.tsx
@@ -1,25 +1,66 @@
 'use client';
 
+import Link from 'next/link';
 import { useState } from 'react';
+import { useUserStore } from '@/configs/store';
+import { Bars4Icon, XMarkIcon, NewspaperIcon, CircleStackIcon, ListBulletIcon, ArrowRightStartOnRectangleIcon } from '@heroicons/react/24/solid';
 
 export const BurgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const uid = useUserStore(state => state.uid);
+  const signout = useUserStore(state => state.signout);
 
   return (
     <div className="ml-auto">
-      <button className="sm:hidden" onClick={() => setIsOpen(true)}>
-        Menu
+      <button
+        className="p-2 sm:hidden"
+        onClick={() => setIsOpen(true)}
+        aria-label="Menu open"
+        type="button"
+      >
+        <Bars4Icon className="h-6 w-6 text-black dark:invert" />
       </button>
 
       {isOpen && (
-        <div className="absolute z-20 left-0 top-0 w-screen h-screen bg-inherit p-5">
-          <button onClick={() => setIsOpen(false)}>Close</button>
+        <div className="absolute right-0 top-0 z-20 h-screen w-screen bg-white p-10 text-black dark:invert">
+          <button
+            onClick={() => setIsOpen(false)}
+            className="ml-auto block bg-transparent p-2"
+            aria-label="Close menu"
+            type="button"
+          >
+            <XMarkIcon className="h-6 w-6 text-inherit" />
+          </button>
 
-          <ul>
-            <li>News</li>
-            <li>Coins</li>
-            <li>Signout</li>
-          </ul>
+          <nav className="mt-5 grid gap-4 font-bold text-xl">
+            <Link href="/news" onClick={() => setIsOpen(false)}>
+              <NewspaperIcon className="h-6 w-6 inline mr-2"/>News
+            </Link>
+
+            <Link href="/coins" onClick={() => setIsOpen(false)}>
+              <CircleStackIcon className="h-6 w-6 inline mr-2"/>Coins
+            </Link>
+
+            {uid && <Link href="/watchlist" onClick={() => setIsOpen(false)}>
+              <ListBulletIcon className="h-6 w-6 inline mr-2"/>Watchlist
+            </Link>}
+
+            {uid ? (
+              <Link
+                href="#"
+                onClick={() => {
+                  signout();
+                  setIsOpen(false);
+                }}
+              >
+                <ArrowRightStartOnRectangleIcon className="h-6 w-6 inline mr-2"/> Signout
+              </Link>
+            ) : (
+              <Link href="/signin" onClick={() => setIsOpen(false)}>
+                Signin
+              </Link>
+            )}
+          </nav>
         </div>
       )}
     </div>
