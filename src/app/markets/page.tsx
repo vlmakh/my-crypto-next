ad17c739cfec14154cd8f98d8bf9be6f9360c4a0
Vlad Makhnenko <vlmakh@gmail.com>
2024-02-22 06:28:45 +0200

    Update page.tsx

--- src/app/markets/page.tsx
+++ src/app/markets/page.tsx
@@ -6,30 +6,43 @@ export default async function MarketDataPage() {
   const result: IMarketData = await fetchMarkets();
 
   return (
-    <div className="py-4 text-center">
-      <h2>
-        Total Cryptomarket Cap: <b>{result.marketCap.toLocaleString()}$</b>{' '}
-        {result.marketCapChange.toFixed(1)}%
-      </h2>
-      <Image
-        src="/images/markets-568x322.webp"
-        alt=""
-        width={568}
-        height={322}
-        className="mx-auto"
-      />
+    <>
+      <div className="relative mx-auto mt-4 max-w-[568px]">
+        <Image
+          src="/images/markets-568x322.webp"
+          alt=""
+          width={568}
+          height={322}
+          className="mx-auto"
+        />
 
-      <h2 className='mt-4'>
-        Bitcoin dominance: <b>{result.btcDominance.toFixed(1)}%</b>{' '}
-        {result.btcDominanceChange.toFixed(1)}%
-      </h2>
-      <Image
-        src="/images/bitcoin_dom.webp"
-        alt=""
-        width={568}
-        height={320}
-        className="mx-auto"
-      />
-    </div>
+        <p className="absolute left-1/2 top-4 -translate-x-1/2 transform font-bold">
+          Total Cryptomarket Cap
+        </p>
+
+        <h2 className="absolute bottom-4 left-1/2 -translate-x-1/2 transform text-3xl font-bold sm:text-5xl">
+          {result.marketCap.toLocaleString()}$
+        </h2>
+
+        {/* <p>{result.marketCapChange.toFixed(1)}%</p> */}
+      </div>
+
+      <div className="relative mx-auto mt-4 max-w-[568px] before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:w-full before:bg-gradient-to-r before:from-transparent before:to-slate-700">
+        <Image
+          src="/images/bitcoin_dom.webp"
+          alt=""
+          width={568}
+          height={320}
+          className="mx-auto"
+        />
+
+        <p className="absolute right-4 top-4 z-20 font-bold">Bitcoin dominance</p>
+
+        <h2 className="absolute right-4 top-1/2 z-20 -translate-y-1/2 transform text-4xl font-bold sm:text-6xl">
+          {result.btcDominance.toFixed(1)}%
+          {/* {result.btcDominanceChange.toFixed(1)}% */}
+        </h2>
+      </div>
+    </>
   );
 }
