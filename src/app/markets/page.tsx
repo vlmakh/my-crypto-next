709d9ad033c0429aa7c3b3d283e8959a05dcbaa9
Vlad Makhnenko <vlmakh@gmail.com>
2024-03-02 20:05:39 +0200

    Update page.tsx

--- src/app/markets/page.tsx
+++ src/app/markets/page.tsx
@@ -1,6 +1,7 @@
 import { fetchMarkets } from '@/utils/fetchCoinList';
 import type { IMarketData } from '@/types';
 import Image from 'next/image';
+import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/16/solid';
 
 export default async function MarketDataPage() {
   const result: IMarketData = await fetchMarkets();
@@ -16,8 +17,18 @@ export default async function MarketDataPage() {
           className="mx-auto"
         />
 
-        <p className="absolute left-1/2 top-4 -translate-x-1/2 transform font-bold">
+        <p className="absolute left-10 top-4 font-bold">
           Total Cryptomarket Cap
+          <span
+            className={`flex ${result.marketCapChange >= 0 ? 'text-green-500' : 'text-red-500'}`}
+          >
+            {result.marketCapChange >= 0 ? (
+              <ChevronUpIcon className="h-6 w-6" />
+            ) : (
+              <ChevronDownIcon className="h-6 w-6" />
+            )}
+            {Math.abs(result.marketCapChange).toFixed(1)}%
+          </span>
         </p>
 
         <h2 className="absolute bottom-4 left-1/2 -translate-x-1/2 transform text-3xl font-bold sm:text-5xl">
@@ -34,7 +45,19 @@ export default async function MarketDataPage() {
           className="mx-auto"
         />
 
-        <p className="absolute right-4 top-4 z-10 font-bold">Bitcoin dominance</p>
+        <p className="absolute right-4 top-4 z-10 font-bold">
+          Bitcoin dominance
+          <span
+            className={`flex justify-end ${result.btcDominanceChange >= 0 ? 'text-green-500' : 'text-red-500'}`}
+          >
+            {result.btcDominanceChange >= 0 ? (
+              <ChevronUpIcon className="h-6 w-6" />
+            ) : (
+              <ChevronDownIcon className="h-6 w-6" />
+            )}
+            {Math.abs(result.btcDominanceChange).toFixed(1)}%
+          </span>
+        </p>
 
         <h2 className="absolute right-4 top-1/2 z-10 -translate-y-1/2 transform text-4xl font-bold sm:text-6xl">
           {result.btcDominance.toFixed(1)}%
