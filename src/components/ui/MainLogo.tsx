5a83265ce340288bfe6610e2f9cab100be4f8082
Vlad Makhnenko <vlmakh@gmail.com>
2024-05-02 18:17:02 +0300

    add text to logo

--- src/components/ui/MainLogo.tsx
+++ src/components/ui/MainLogo.tsx
@@ -8,15 +8,19 @@ export const MainLogo = () => {
   const currencyName = useCurrencyStore(state => state.currency.name);
 
   return (
-    <Link href={`/?page=1&currency=${currencyName}`} className="group py-2">
+    <Link
+      href={`/?page=1&currency=${currencyName}`}
+      className="group mr-4 flex items-center gap-2 py-2"
+    >
       <Image
         src="/logo512.webp"
         alt="Logo"
         width={40}
         height={40}
-        className="mr-5 transition-transform group-hover:scale-110"
+        className="transition-transform group-hover:scale-110"
         priority
-      />
+      />{' '}
+      <span className="hidden lg:block">MyCrypto</span>
     </Link>
   );
 };
